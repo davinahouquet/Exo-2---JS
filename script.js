f601c4e33b293d0b251ffa29640d3d9e0fe4577b
davinahouquet <davina.houquet@hotmail.com>
2023-04-14 14:59:59 +0200

    Correction margin h1, h2

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const carre = document.createElement("div")
 carre.classList.add("carre")
 
-const board = document.querySelector("#board")
+const square = document.querySelector("#square")
 
 for (let i = 1; i <= 4; i++) {
     const newCarre = carre.cloneNode();
-    
+
     newCarre.innerText = i
-    board.appendChild(newCarre)
+    square.appendChild(newCarre)
 
     newCarre.addEventListener("click", function() {
         if(newCarre.classList.contains("clicked")) {
